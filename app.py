0fcfec0af0c21bdde71055b77468824348fdfb6d
Murilo  M. Castro <123572431+playerking3@users.noreply.github.com>
2024-03-11 17:24:24 -0300

    banco de dados

--- app.py
+++ app.py
@@ -1,23 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, flash, url_for, request
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/livro'
+db = SQLAlchemy(app)
 
-lista = ['Far Cry 3',
-         'Valorant',
-         'Brawlhalla',
-         'Counter Strike',
-         'Bloons TD6', 'Stumble Guys',
-         'Overcooked 2', 'Life is Strange',
-         'Days Gone']
-
-
+class Livro(db.Model):
+    id_livro = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    titulo = db.Column(db.String(100))
+    autor = db.Column(db.String(254))
+    ano_publicacao = db.Column(db.Integer)
 
 @app.route('/')
 def index():
-    return 'olá mundo'
+    livros = Livro.query.all()
+    return render_template('cadastro_livros.html', outro=livros)
 
-@app.route('/games')
-def games():
-    return render_template('index.html', listaJogo=lista)
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Livro')
+
+@app.route('/criar', methods=['POST'])
+def criar():
+    titulo = request.form['titulo']
+    autor = request.form['autor']
+    ano_publicacao = request.form['ano_publicacao']
+
+    livro = Livro.query.filter_by(titulo=titulo).first()
+    if livro:
+        flash('livro jpa existente')
+        return redirect(url_for('novo'))
+
+    novo_livro = Livro(titulo=titulo, autor=autor, ano_publicacao=ano_publicacao)
+    db.session.add(novo_livro)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
