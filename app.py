318a51f7c9c75b8c5de9f383da4f79042eb886c0
Murilo  M. Castro <123572431+playerking3@users.noreply.github.com>
2024-03-18 16:07:06 -0300

    bug consertado

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, flash, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'ZGBKJHFGNKFLGERHN'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/livro'
 db = SQLAlchemy(app)
 
